85d6792f2c4fc5363bb9ca1e24010be025c4ac67
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2019-10-20 16:12:48 +0200

    Initial Commit

---
+++ SwipeTabViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeTabViewController
+//
+//  Created by Levchuk Misha on 20/10/2019.
+//  Copyright © 2019 Levchuk Misha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
